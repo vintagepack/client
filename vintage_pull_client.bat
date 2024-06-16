5f281e0683ce5762ac77b9ff2499eb0362dead53
PowfuArras <>
2024-06-16 12:08:58 -0500

    please push uwu

---
+++ vintage_pull_client.bat
@@ -0,0 +1,22 @@
+@echo off
+setlocal
+
+set /p CONFIRM=Are you sure you want to update? This will delete all worlds and screenshots, so go make backups if you havn't already! ^
+(type 'y' to confirm):
+
+set "GIT_REPO=https://github.com/vintagepack/client.git"
+
+git init .
+git remote get-url origin 2>nul || git remote add origin %GIT_REPO%
+
+if /i "%CONFIRM%"=="y" (
+	git fetch origin
+	git switch main
+	git reset --hard origin/main
+	git clean -fdx
+) else (
+	echo You have cancelled updating.
+	pause
+)
+
+endlocal
